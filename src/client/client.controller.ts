b5422391c5abf0aea8156353bc4a3f8628a777f4
Diego <abastosdiego@gmail.com>
2025-03-06 17:30:49 -0300

    Criação do projeto e do módulo clients

---
+++ src/client/client.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { ClientRepository } from "./client.repository";
+
+@Controller('/clients')
+export class ClientController {
+
+    constructor(private clientRepository: ClientRepository) {}
+
+    @Post()
+    async create(@Body() clientData: any) {
+        this.clientRepository.create(clientData);
+        return clientData;
+    }
+
+    @Get()
+    async listUsuarios() {
+        return this.clientRepository.list();
+    }
+
+}
